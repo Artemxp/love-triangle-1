1dbe0fbf00078675c650f05c7bd1ae1c14f4b150
Artsiom <artem.ice33@gmail.com>
2019-03-01 21:34:40 +0300

    feat: add function getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count=0, love = 0;
+    for(let i=0; i<preferences.length; i++) {
+        love = (preferences[(preferences[(preferences[i]-1)]-1)]-1);
+        love==i?count++:null;
+    }
+    return Math.floor(count / 3);
 };
+
+
+
